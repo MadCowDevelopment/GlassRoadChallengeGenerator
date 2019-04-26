7ce15d4b2fcfde3c37f08122bcab7cd074537a30
Michael Gailer <mgr@mm-software.com>
2019-04-26 14:21:00 +0800

    added project stub

---
+++ grcg/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace grcg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey(false);
+        }
+    }
+}
